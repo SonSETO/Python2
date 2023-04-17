835c3fe127500125eb3a8f5dd38be0da5e80945a
SonSETO <yesthis9090@gmail.com>
2023-04-17 22:42:33 +0900

    day01

---
+++ DAY01/Ex04-readHello.py
@@ -0,0 +1,14 @@
+'''
+ 파일명 :Ex04-readHello.py
+ open 함수모드
+     r(read mode) : 읽기 전용 모드 / 파일 없으면 에러
+'''
+'''
+file = open('hello.txt', 'rt', encoding='UTF-8')
+str = file.read()
+print(str, end='')
+file.close()
+'''
+with open('hello.txt', 'rt',encoding= ' UTF-8') as file :
+    str = file.read()
+    print(str, end='')
