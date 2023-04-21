786ae29dd8b7f6879288eda6fd4367edf4adef47
SonSETO <yesthis9090@gmail.com>
2023-04-21 21:48:14 +0900

    112

--- DAY04/Ex01-Linear.py
+++ DAY04/Ex01-Linear.py
@@ -14,7 +14,42 @@ class LinearList():
         lLen = len(linear) # 리스트의 현재 크기   
         linear[lLen - 1] = data # 리스트에 데이터 삽입
 
+# 삽입
+    def insert_data(self, position, data) :
+        linear = self.linear
+        
+        if position < 0 or position> len(linear): #유효성 검사
+            print('데이터를 삽입할 범위를 벗어났습니다.')
+            return
+        
+        linear.append(None) # 빈칸 추가
+        linearSize = len(linear) # 리스트의 현재 크기
+        
+        # 삽입 위치 이후 요소들을 한칸씩 뒤로 이동
+        for i in range(linearSize - 1, position, -1):
+            linear[i] = linear[i -1]
+            linear[i -1] = None
+         
+        linear[position] = data # 리스트에 데이터 삽입 
 
+# 삭제
+    def delete_data(self, postion): # 리스트에서 데이터 삭제
+        linear = self.linear
+        
+        if postion <0 or postion > len(linear) : # 유효성 검사
+            print('데이터를 삽입할 범위를 벗어났습니다.')
+
+        linear[postion] = None # 해당위치 데이터 삭제
+        linearSize = len(linear)
+        
+        # 삭제 이후 요소들 한칸씩 앞으로 이동
+        for i in range(postion + 1, linearSize):
+            linear[i - 1] = linear[i]
+            linear[i] = None
+            
+        del(linear[linearSize - 1]) # 빈칸제거    
+
+    
     def print_list(self):
         linear = self.linear
         for list in linear:
@@ -28,9 +63,9 @@ linear.add_data(4)
 linear.add_data(2)
 linear.add_data(6)
 
+linear.insert_data(3, 99)
 
-
-#linear.insert_data(3, 99)
+linear.delete_data(2)
 
 linear.print_list(); # 리스트 출력
     
