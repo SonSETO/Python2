4c505f8ddee13b275a0871f4fcece3c6c2068e77
SonSETO <yesthis9090@gmail.com>
2023-04-25 21:45:25 +0900

    123

--- DAY04/Ex07-Heap.py
+++ DAY04/Ex07-Heap.py
@@ -22,10 +22,37 @@ heap = MinHeap()
 heap.push(3)
 heap.push(1)
 heap.push(4)
-heap.push(1)
+heap.push(2)
+
+print('=== MinHeap ===')
 
 print(heap.pop())
 print(heap.pop()) 
 print(heap.pop()) 
 print(heap.pop()) 
             
+            
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+        
+    def push(self, val):
+        heapq.heappush(self.heap, -val)
+
+    def pop(self):
+        return -heapq.heappop(self.heap)
+                        
+# 실행코드
+heap = MaxHeap()
+heap.push(3)
+heap.push(1)
+heap.push(4)
+heap.push(2)
+
+
+print('=== MaxHeap ===')
+print(heap.pop())
+print(heap.pop()) 
+print(heap.pop()) 
+print(heap.pop())                         
+
